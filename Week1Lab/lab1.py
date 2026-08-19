bf2ba5e0b60ac784d3fb88afda93b4e58f96c42d
STalamantes1 <STalamantes1@csi.edu>
2026-08-19 14:35:01 -0600

    added lab folder and added some code

---
+++ Week1Lab/lab1.py
@@ -0,0 +1,9 @@
+# This is a comment and it doesnt effeft my program 
+# name=input("What is your name? ")
+# print("Hello, ", name)
+
+# age=input("Enter your age: ")
+# print("you are ",age," years old")
+
+favorite_food = input("What is your favorite food? ")
+print("You said your favorite food is:", favorite_food)
