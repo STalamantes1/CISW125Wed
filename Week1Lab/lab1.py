f28b12a78a68cb4cdf185e658291deed0c4137ac
STalamantes1 <STalamantes1@csi.edu>
2026-09-02 13:30:58 -0600

    Add lab1.py and Variables and Expressions Lab (1).py with initial variable examples and comments

--- Week1Lab/lab1.py
+++ Week1Lab/lab1.py
@@ -1,9 +1,61 @@
-# This is a comment and it doesnt effeft my program 
-# name=input("What is your name? ")
-# print("Hello, ", name)
+#Name variables in relation to what their value is
+game="Minecraft"
+#Print is what shows program output
+print(game)
 
-# age=input("Enter your age: ")
-# print("you are ",age," years old")
+character="Sebastian"
+#if you want number values do not put integer in between ""
+score=5
 
-favorite_food = input("What is your favorite food? ")
-print("You said your favorite food is:", favorite_food)
+#Variable names should describe what they contain
+
+#bad examples of Variable names
+x=50
+a="minecraft"
+thing="Sebastian"
+
+#Variable Rules 101: 
+#Variable Names cannot contain spaces
+amount_of_oranges=10 
+#Cannot begin with a number 
+
+#Should usually start with a lowercase
+#Use underscores to seperate words
+
+
+
+print("I like the game", game, "Because it is fun")
+
+print(f"My fav game is {game} because it is fun")
+
+
+
+favfood="Pizza"
+print(f"My fav food is {favfood}")
+
+total=5+3
+print(total)
+
+
+
+addition=5+5 #output would be 10
+subtraction=5-5 #Output would 0
+multiplication=5*5 #Output is 25
+division=10/5 #output is 2
+
+#+ is for addition
+# - is for subtraction 
+# * is for multiplication 
+# \ is for division 
+# % is for showing the remainder, % is called a modulus 
+# ** is for exponents
+# // is for floor division
+
+number=10//3
+print(number)
+
+fname="Sebastian "
+lname="Talamantes"
+#If we want to add strings together is it called String Concatenation 
+full_name=fname+lname
+print(full_name)
---
+++ Week3/week3lecture.py
@@ -0,0 +1,17 @@
+# #Whats the difference between int and float? 
+# #Int is whole numbers, no decimal point (10,2,5,6,7,)
+# #Float contains decimals(10.25,12.058,12.52)
+
+
+# x=15 #int value 
+
+# y=2 #float
+
+# print(x,type(x))
+# print(y,type(y))
+
+# print(x//y) #// floor division, will give whole number value 
+# print(x/y) #/ regular division, this will give float(decimal) value
+
+user_text=input("Type something: ")
+print(f"Hello, {user_text}")
